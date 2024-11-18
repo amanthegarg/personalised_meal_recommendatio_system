3c3af6cce3b04a3511c4812fd1f568b2cb980aa2
amanthegarg <gargaman2003@gmail.com>
2024-11-18 21:16:10 +0530

    change of name

--- Streamlit_Frontend/Hello.py
+++ Streamlit_Frontend/Hello.py
@@ -10,8 +10,7 @@ st.write("# Welcome to Diet Recommendation System! 👋")
 st.sidebar.success("Select a recommendation app.")
 
 st.markdown(
-    """
-    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
-    You can find more details and the whole project on my [repo](https://github.com/zakaria-narjis/Diet-Recommendation-System).
+ """
+this is a minor project for personalised meal recommendation system
     """
 )
